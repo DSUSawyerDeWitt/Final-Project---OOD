8849952f81fc4a73f289565be2665d8cae246351
DSUSawyerDeWitt <Sawyer.DeWitt@trojans.dsu.edu>
2024-04-18 20:22:58 -0500

    Enemy Movement Off Screen
    Enemy will now come back when they wonder off screen.

--- Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -10,16 +10,22 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed;
 
+    [SerializeField]
+    private float _screenBorder;
+
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
-    // Start is called before the first frame update
+    private float _changeDirectionCooldown;
+    private Camera _camera;
+
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
         _targetDirection = transform.up;
-
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
@@ -32,16 +38,50 @@ public class EnemyMovement : MonoBehaviour
 
     private void UpdateTargetDirection()
     {
-        if(_playerAwarenessController.AwareOfPlayer)
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+        HandleEnemyOffScreen();
+    }
+
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCooldown -= Time.deltaTime;
+
+        if(_changeDirectionCooldown <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            _targetDirection = rotation * _targetDirection;
+
+            _changeDirectionCooldown = Random.Range(1f, 5f);
+        }
+    }
+
+    private void HandlePlayerTargeting()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
         {
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
         }
     }
 
-    private void HandleRandomDirectionChange()
+    private void HandleEnemyOffScreen()
     {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
 
+        if((screenPosition.x < _screenBorder && _targetDirection.x < 0 ||
+            (screenPosition.x > _camera.pixelWidth - _screenBorder && _targetDirection.x > 0)))
+        {
+            _targetDirection = new Vector2(-_targetDirection.x, _targetDirection.y);
+        }
+
+        if((screenPosition.y < _screenBorder && _targetDirection.y < 0 ||
+            (screenPosition.y > _camera.pixelHeight - _screenBorder && _targetDirection.y > 0)))
+        {
+            _targetDirection = new Vector2(_targetDirection.x, - _targetDirection.y);
+        }
     }
+
     private void RotateWowardsTarget()
     {
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    //add camera bullet deletion.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<EnemyMovement>())
