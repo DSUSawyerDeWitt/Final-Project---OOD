f110001125ba21b4747ca3aa3a7d66fb5e1f0fab
DSUSawyerDeWitt <Sawyer.DeWitt@trojans.dsu.edu>
2024-04-23 18:28:43 -0500

    Main Menu
    Added a Main Menu

---
+++ Assets/Scripts/Game/Collectables/CollectableSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _collectablePrefab;
+
+    public void SpawnCollectable(Vector2 position)
+    {
+        int index = Random.Range(0, _collectablePrefab.Count);
+        var selectedCollectable = _collectablePrefab[index];
+
+        Instantiate(selectedCollectable, position, Quaternion.identity);
+    }
+}
